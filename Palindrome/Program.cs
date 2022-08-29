3d2f188a4fe595d50b90cd711f19a844d78d2073
Serkan Parlak <@GTUSR0306>
2022-08-29 15:17:52 +0300

    parse solutions

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -11,37 +11,5 @@
  */
 var asd = new[] { 1, 2, 2, 1 };
 var linked = asd.Aggregate<int, ListNode>(null, (list, current) => new ListNode(current, list));
-Console.WriteLine(Solution.IsPalindrome(linked));
-
-public static class Solution
-{
-    public static bool IsPalindrome(ListNode head)
-    {
-        var arr = new List<int>();
-        ListNode active = head;
-        while (active != null)
-        {
-            arr.Add(active.val);
-            active = active.next;
-        }
-        for (int i = 0; i < Math.Floor(arr.Count / 2d); i++)
-        {
-            if (arr[i] != arr[arr.Count - 1 - i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-public class ListNode
-{
-    public int val;
-    public ListNode? next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
+// solution 1
+Console.WriteLine(Solution1.IsPalindrome(linked));
---
+++ Palindrome/Solutions/Solution1.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution1
+{
+    public static bool IsPalindrome(ListNode head)
+    {
+        var arr = new List<int>();
+        ListNode active = head;
+        while (active != null)
+        {
+            arr.Add(active.val);
+            active = active.next;
+        }
+        for (int i = 0; i < Math.Floor(arr.Count / 2d); i++)
+        {
+            if (arr[i] != arr[arr.Count - 1 - i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
