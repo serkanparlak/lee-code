92f3aaf5369cc2bf3eba543d1e61f340a0b05716
Serkan Parlak <@GTUSR0306>
2022-08-31 00:01:26 +0300

    solved fizzbizz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(Solution.FizzBuzz(4));
+
+public class Solution
+{
+    public static IList<string> FizzBuzz(int n)
+    {
+        var list = new List<string>();
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                list.Add("FizzBuzz");
+            else if (i % 3 == 0)
+                list.Add("Fizz");
+            else if (i % 5 == 0)
+                list.Add("Buzz");
+            else
+                list.Add(i.ToString());
+        }
+        return list;
+    }
+}
