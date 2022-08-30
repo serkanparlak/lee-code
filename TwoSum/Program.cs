561621cc2ac4103d74aade2d5ec19b8852c1f8c7
Serkan Parlak <@GTUSR0306>
2022-08-31 00:26:21 +0300

    two sum okey

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(Solution.TwoSum(new[] { 3, 2, 4 }, 6));
+
+public class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { };
+    }
+}
