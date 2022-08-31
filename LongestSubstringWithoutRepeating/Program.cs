7ed885b6a9477b129bc0aa972de0dd0e92da604c
Serkan Parlak <@GTUSR0306>
2022-08-31 18:15:48 +0300

    worked

--- LongestSubstringWithoutRepeating/Program.cs
+++ LongestSubstringWithoutRepeating/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(Solution.LengthOfLongestSubstring("pwwkew"));
+
+
+public class Solution
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        var record = 0;
+        var bag = new List<char>();
+        foreach (var c in s)
+        {
+            if (!bag.Contains(c))
+                bag.Add(c);
+            else
+            {
+                if (record < bag.Count)
+                    record = bag.Count;
+                var index = bag.IndexOf(c);
+                for (int i = 0; i <= index; i++)
+                {
+                    bag.RemoveAt(0);
+                }
+                bag.Add(c);
+            }
+        }
+        if (record < bag.Count)
+            record = bag.Count;
+        return record;
+    }
+}
